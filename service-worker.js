cdded330ef2b27f766d99f4483b590a164a724a4
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2020-11-03 01:06:50 +0700

    COMMIT : Codelabs News Later with Indexed DB ready

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,31 @@
 const CACHE_NAME = "firstpwa-v1";
-
 var urlsToCache = [
-    "/",
-    "/icon.png",
-    "/nav.html",
-    "/index.html",
-    "/article.html",
-    "/manifest.json",
-    "/pages/home.html",
-    "/pages/about.html",
-    "/pages/contact.html",
-    "/css/materialize.min.css",
-    "/js/materialize.min.js",
-    "/js/nav.js",
-    "/js/api.js"
-  ]; // list of page that content Assets, JS, and styles
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/article.html",
+  "/pages/home.html",
+  "/pages/about.html",
+  "/pages/contact.html",
+  "/css/materialize.min.css",
+  "/js/materialize.min.js",
+  "/manifest.json",
+  "/js/nav.js",
+  "/js/api.js",
+  "/icon.png"
+];
 
-
-// Install static Cache to ServiceWorker
 self.addEventListener("install", function(event) {
-    event.waitUntil(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
- 
-// Using aset from cache
 self.addEventListener("fetch", function(event) {
-  var base_url = "https://readerapi.codepolitan.com/articles";
+  var base_url = "https://readerapi.codepolitan.com/";
+
   if (event.request.url.indexOf(base_url) > -1) {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
@@ -42,13 +38,12 @@ self.addEventListener("fetch", function(event) {
   } else {
     event.respondWith(
       caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-          return response || fetch (event.request);
+        return response || fetch (event.request);
       })
     )
   }
-}); 
+});
 
-// Deleting older cache
 self.addEventListener("activate", function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
